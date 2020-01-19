3ca25909680ba2e7f4c11f2770eae24bfb4edf0a
Hassaan Abbasi <hassaan.abbasi@ryerson.ca>
2020-01-19 04:05:45 -0500

    Embedded bokeh chart onto home.html webpage with Flask.

--- main.py
+++ main.py
@@ -1,77 +1,86 @@
-from pandas_datareader import data
-import datetime
-from bokeh.plotting import figure, show, output_file
-from bokeh.models import Range1d
+from flask import Flask, render_template
 
-#Establishing dates for usage
-#NOTE: The earliest data available through pandas_datareader is from 1970 - 1 - 1
-start = datetime.datetime(1970, 1, 1)
-present = datetime.date.today()
+app = Flask(__name__)
 
-#Acquiring stock data from Yahoo Finance for Scotiabank
-dataF = data.DataReader("BNS.TO", "yahoo", start, present)
+@app.route("/")
+def home():
+    from pandas_datareader import data
+    import datetime
+    from bokeh.plotting import figure, show, output_file
+    from bokeh.models import Range1d
+    from bokeh.embed import components
+    from bokeh.resources import CDN
 
-#Adding column indicating increase or decrease
-def incOrDec(closing, opening):
-    stock = ""
-    if opening < closing:
-        stock = "Increase"
-    elif closing < opening:
-        stock = "Decrease"
-    else:
-        stock = "Equal"
-    return stock
+    #Establishing dates for usage
+    #NOTE: The earliest data available through pandas_datareader is from 1970 - 1 - 1
+    start = datetime.datetime(1970, 1, 1)
+    present = datetime.date.today()
 
-dataF["Status"] = [incOrDec(closing, opening) for closing, opening in zip(dataF.Close, dataF.Open)]
-dataF["Mid"] = (dataF.Open + dataF.Close) / 2
-dataF["Height"] = abs(dataF.Close - dataF.Open)
+    #Acquiring stock data from Yahoo Finance for Scotiabank
+    dataF = data.DataReader("BNS.TO", "yahoo", start, present)
 
-#Converting 12 hours to ms
-hours = 12 * 60 * 60 * 1000 
+    #Adding column indicating increase or decrease
+    def incOrDec(closing, opening):
+        stock = ""
+        if opening < closing:
+            stock = "Increase"
+        elif closing < opening:
+            stock = "Decrease"
+        else:
+            stock = "Equal"
+        return stock
 
-#Initializing Bokeh chart
-plot = figure(x_axis_type = "datetime", width = 1600, height = 600, x_axis_label = "Date",
-y_axis_label = "Stock Value", sizing_mode = "scale_width")
-plot.grid.grid_line_alpha = 0.3
-plot.title.text_font_size = '15pt'
-plot.xaxis.axis_label_text_font_size = "12pt"
-plot.yaxis.axis_label_text_font_size = "12pt"
-plot.title.text = "Scotiabank Stocks Candlestick Chart"
-plot.legend.location = "top_right"
-plot.legend.click_policy="hide"
+    dataF["Status"] = [incOrDec(closing, opening) for closing, opening in zip(dataF.Close, dataF.Open)]
+    dataF["Mid"] = (dataF.Open + dataF.Close) / 2
+    dataF["Height"] = abs(dataF.Close - dataF.Open)
 
-#Finding the last day of any month
-date = datetime.datetime(dataF.index[-1].year, dataF.index[-1].month, 1)
-next_month = date.replace(day = 28) + datetime.timedelta(days = 4)
-lastDayOfMonth = next_month - datetime.timedelta(days = next_month.day)
+    #Converting 12 hours to ms
+    hours = 12 * 60 * 60 * 1000 
 
-#Finding range of stock data for the month
-year = str(dataF.index[-1].year)
-month = str(dataF.index[-1].month)
+    #Initializing Bokeh chart
+    plot = figure(x_axis_type = "datetime", width = 1800, height = 600, x_axis_label = "Date",
+    y_axis_label = "Stock Value", sizing_mode = "scale_width")
+    plot.grid.grid_line_alpha = 0.3
+    plot.title.text_font_size = '15pt'
+    plot.xaxis.axis_label_text_font_size = "12pt"
+    plot.yaxis.axis_label_text_font_size = "12pt"
+    plot.title.text = "Scotiabank Stocks Candlestick Chart"
 
-rngLow = dataF.loc[f"{year}-{month}-1" : f"{year}-{month}-{lastDayOfMonth.day}", "Low"]
-rngHigh = dataF.loc[f"{year}-{month}-1" : f"{year}-{month}-{lastDayOfMonth.day}", "High"]
+    #Finding the last day of any month
+    date = datetime.datetime(dataF.index[-1].year, dataF.index[-1].month, 1)
+    next_month = date.replace(day = 28) + datetime.timedelta(days = 4)
+    lastDayOfMonth = next_month - datetime.timedelta(days = next_month.day)
 
-#Starting graph at first day of latest month to last day of month
-plot.y_range = Range1d(min(rngLow) - 0.1, max(rngHigh) + 0.1)
-plot.x_range = Range1d(datetime.datetime(dataF.index[-1].year, dataF.index[-1].month, 1), 
-datetime.datetime(dataF.index[-1].year, dataF.index[-1].month, lastDayOfMonth.day))
+    #Finding range of stock data for the month
+    year = str(dataF.index[-1].year)
+    month = str(dataF.index[-1].month)
 
-#Stock high and low line plot
-plot.segment(dataF.index, dataF.High, dataF.index, dataF.Low, color = "black")
+    rngLow = dataF.loc[f"{year}-{month}-1" : f"{year}-{month}-{lastDayOfMonth.day}", "Low"]
+    rngHigh = dataF.loc[f"{year}-{month}-1" : f"{year}-{month}-{lastDayOfMonth.day}", "High"]
 
-#Stock rises plot
-plot.rect(x = dataF.index[dataF.Status == "Increase"], y = dataF.Mid[dataF.Status == "Increase"], width = hours, 
-height = dataF.Height[dataF.Status == "Increase"], color = "#19c902")
+    #Starting graph at first day of latest month to last day of month
+    plot.y_range = Range1d(min(rngLow) - 0.1, max(rngHigh) + 0.1)
+    plot.x_range = Range1d(datetime.datetime(dataF.index[-1].year, dataF.index[-1].month, 1), 
+    datetime.datetime(dataF.index[-1].year, dataF.index[-1].month, lastDayOfMonth.day))
 
-#Stock falls plot
-plot.rect(x = dataF.index[dataF.Status == "Decrease"], y = dataF.Mid[dataF.Status == "Decrease"], width = hours, 
-height = dataF.Height[dataF.Status == "Decrease"], color = "#db3125")
+    #Stock high and low line plot
+    plot.segment(dataF.index, dataF.High, dataF.index, dataF.Low, color = "black")
 
-#Stock equal plot
-plot.rect(x = dataF.index[dataF.Status == "Equal"], y = dataF.Mid[dataF.Status == "Equal"], width = hours, 
-height = dataF.Height[dataF.Status == "Equal"], color = "black")
+    #Stock rises plot
+    plot.rect(x = dataF.index[dataF.Status == "Increase"], y = dataF.Mid[dataF.Status == "Increase"], width = hours, 
+    height = dataF.Height[dataF.Status == "Increase"], color = "#19c902")
 
-#Output
-output_file("chart.html")
-show(plot)
+    #Stock falls plot
+    plot.rect(x = dataF.index[dataF.Status == "Decrease"], y = dataF.Mid[dataF.Status == "Decrease"], width = hours, 
+    height = dataF.Height[dataF.Status == "Decrease"], color = "#db3125")
+
+    #Stock equal plot
+    plot.rect(x = dataF.index[dataF.Status == "Equal"], y = dataF.Mid[dataF.Status == "Equal"], width = hours, 
+    height = dataF.Height[dataF.Status == "Equal"], color = "black")
+
+    script, div = components(plot)
+    cdnJS = CDN.js_files[0]
+
+    return render_template("home.html", script = script, div = div, cdnJS = cdnJS)
+if (__name__ == "__main__"):
+    app.run(debug = True)
